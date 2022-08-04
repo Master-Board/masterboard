698a33c9d5331cecfb192efaf999368656515ef2
chk223 <104356399+chk223@users.noreply.github.com>
2022-08-05 00:47:35 +0900

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,16 +2,23 @@ require("dotenv").config()
 const express = require("express")
 const http = require("http")
 const app = express()
+app.set('port', process.env.PORT || 8005);
 const server = http.createServer(app)
 const PORT = process.env.PORT
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        credential:true
+    }
+})
 app.use(express.urlencoded({extended: true}))
-
+const moment = require('moment')
 // const api = require('./apis/apis')
 // app.use("/", api)
 const cors = require("cors")
 app.use(cors({
-    origin: 'ec2-13-125-172-64.ap-northeast-2.compute.amazonaws.com:3000', // 출처 허용 옵션
+    //origin: 'ec2-13-125-172-64.ap-northeast-2.compute.amazonaws.com:3000', // 출처 허용 옵션
+    origin: 'localhost:3000', // 출처 허용 옵션
     credential: 'true' // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
 }));
 function init_cluedeck(){
@@ -140,5 +147,9 @@ io.on('connection',(socket) => {
         }
     })
 })
+
+// const SocketIO = require('./socket/socket.js')
+// SocketIO(server)
+
 
 server.listen(PORT, () => console.log(`server is runnig ${PORT}`))
