0e6db6d49a0c11d1590c3d6ec998033e66b63f0a
JuHyeongJin <wngudwlswkd@gmail.com>
2022-08-08 17:00:23 +0900

    Add chatting modal onoff, onkeypress

--- client/src/routes/Deception.jsx
+++ client/src/routes/Deception.jsx
@@ -10,10 +10,12 @@ function Deception(props) {
   const [user, setUser] = useState('');
   const [users, setUsers] = useState('');
   const [room, setRoom] = useState('');
+  const [chatting, setChatting] = useState(false);
   const [message, setMessage] = useState('');
   const [text, setText] = useState('');
   const [messages, setMessages] = useState([{name: "형진", message: "hi"}, {name: "형진", message: "hello"}]);
   const { roomNumber } = useParams();
+
   
   useEffect(() => {
     setUser(props.user.id);
@@ -50,20 +52,31 @@ function Deception(props) {
   return (
       <div id="deception" style={{background: 'url("img/background.webp")', backgroundSize: 'cover', width: '100%', height: '100%'}}>
         {user}님 어서오세요! #{room} 디셉션 방입니다
+        <button onClick={()=>setChatting(!chatting)}>채팅</button>
         <button onClick={Disconnect}><Link to="/mainpage">나가기</Link></button>
-        <div style={{width: "300px", height: "600px", border: "2px solid #111"}}>
+        {chatting === true ? 
+          <div style={{width: "300px", height: "600px", border: "2px solid #111"}}>
           <div style={{backgroundColor: "#eee"}}>Room : {room}</div>
-          <div className="messages" style={{width: "290px", height: "540px", border: "1px solid #111"}}>
+          <div className="messages" style={{width: "290px", height: "540px", border: "1px solid #111", overflow: "scroll"}}>
             {messages.map((data, i) => (
               <div key={i} >
                 <div>{data.name} : {data.message}</div>
               </div>
             ))}
           </div>
-          <input type="text" placeholder="메세지를 입력하세요" onChange={(e)=>setText(e.target.value)} value={text} />
-          <button onClick={(e)=>{
-            sendMessage();
-          }}>send</button>
+          <input type="text" placeholder="메세지를 입력하세요" onChange={(e)=>setText(e.target.value)} value={text} onKeyPress={(e)=>(e.key === "Enter" ? sendMessage() : null)} />
+          <button onClick={()=>sendMessage()}>send</button>
+        </div> : null}
+        <div className='gameboard'>
+          <div className='top'>
+            
+          </div>
+          <div className='middle'>
+
+          </div>
+          <div className='bottom'>
+
+          </div>
         </div>
       </div>
     );
