2a4002081e06bef98aed2c91200ec2099746dc45
JuHyeongJin <wngudwlswkd@gmail.com>
2022-08-10 19:00:23 +0900

    Render user's data on card

--- client/src/routes/Deception.jsx
+++ client/src/routes/Deception.jsx
@@ -9,14 +9,15 @@ const socket = io(ENDPOINT);
 function Deception(props) {
 
   const [user, setUser] = useState('');
-  const [users, setUsers] = useState('');
   const [room, setRoom] = useState('');
   const [chatting, setChatting] = useState(false);
   const [message, setMessage] = useState('');
   const [text, setText] = useState('');
   const [messages, setMessages] = useState([{name: "형진", message: "hi"}, {name: "형진", message: "hello"}]);
   const { roomNumber } = useParams();
+  let userIndex=1;
 
+  const [users, setUsers] = useState([{name: "창현", blue: [12, 42, 53, 13], red: [2, 43, 25, 83]}, {name: "형진", blue: [54, 62, 11, 40], red: [1, 3, 4, 5]}, {name: "민호", blue: [6, 7, 8, 9], red: [6, 7, 8, 9]}, {name: "철", blue: [10, 11, 12, 13], red: [14, 15, 16, 17]}]);
   
   useEffect(() => {
     setUser(props.user.id);
@@ -36,6 +37,17 @@ function Deception(props) {
     });
 
   }, [messages]);
+
+  useEffect(() => {
+    // 유저 받기
+    // socket.on("유저받기", (data) => {
+    //   setUsers(data)
+    // })
+    for(let i = 0; i < users.length; i++){
+      if(users[i].name == user) userIndex = i;
+    }
+
+  }, [users]);
 
   useEffect(() => {
     setMessage({name: user, message: text, room: room});
@@ -71,25 +83,25 @@ function Deception(props) {
             </div> : null}
           <div className='gameboard' style={{textAlign: "center", width: "1540px", height: "690px"}}>
             <div className='top' style={{display: "flex"}}>
-              <DeceptionUser/>
-              <DeceptionUser/>
-              <DeceptionUser/>
-              <DeceptionUser/>
-              <DeceptionUser/>
+              <DeceptionUser user={users[(userIndex+6)%(users.length)]}/>
+              <DeceptionUser user={users[(userIndex+1)%(users.length)]}/>
+              <DeceptionUser user={users[(userIndex+2)%(users.length)]}/>
+              <DeceptionUser user={users[(userIndex+3)%(users.length)]}/>
+              <DeceptionUser user={users[(userIndex+7)%(users.length)]}/>
             </div>
             <div className='middle' style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-              <DeceptionUser/>
+              <DeceptionUser user={users[(userIndex+4)%(users.length)]}/>
               <div style={{width: "800px", height: "250px", margin: "0px 55px", border: "1px solid #111"}}>
                 share
               </div>
-              <DeceptionUser/>
+              <DeceptionUser user={users[(userIndex+5)%(users.length)]}/>
             </div>
             <div className='bottom' style={{display: "flex"}}>
-              <DeceptionUser/>
-              <DeceptionUser/>
-              <DeceptionUser/>
-              <DeceptionUser/>
-              <DeceptionUser/>
+              <DeceptionUser user={users[(userIndex+8)%(users.length)]}/>
+              <DeceptionUser user={users[(userIndex+10)%(users.length)]}/>
+              <DeceptionUser user={users[userIndex]}/>
+              <DeceptionUser user={users[(userIndex+11)%(users.length)]}/>
+              <DeceptionUser user={users[(userIndex+9)%(users.length)]}/>
             </div>
           </div>
         </body>
--- client/src/routes/DeceptionUser.jsx
+++ client/src/routes/DeceptionUser.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 
 function DeceptionUser(props) {
+
   return (
     <div style={{width: "270px", height: " 180px", border: "1px solid #111", margin: "20px 17px"}}>
-        <div style={{margin: "5px 0px"}}>DeceptionUser</div>
+        <div style={{margin: "5px 0px"}}>{props.user.name}</div>
         <div className='단서' style={{display:"flex"}}>
-            <div style={{width: "60px", height: "60px", border: "1px solid #000", margin: "5px 5px"}}>1</div>
-            <div style={{width: "60px", height: "60px", border: "1px solid #000", margin: "5px 5px"}}>2</div>
-            <div style={{width: "60px", height: "60px", border: "1px solid #000", margin: "5px 5px"}}>3</div>
-            <div style={{width: "60px", height: "60px", border: "1px solid #000", margin: "5px 5px"}}>4</div>
+            <div style={{width: "60px", height: "60px", border: "1px solid #000", margin: "5px 5px"}}>{props.user.blue[0]}</div>
+            <div style={{width: "60px", height: "60px", border: "1px solid #000", margin: "5px 5px"}}>{props.user.blue[1]}</div>
+            <div style={{width: "60px", height: "60px", border: "1px solid #000", margin: "5px 5px"}}>{props.user.blue[2]}</div>
+            <div style={{width: "60px", height: "60px", border: "1px solid #000", margin: "5px 5px"}}>{props.user.blue[3]}</div>
         </div>
         <div className='수단' style={{display:"flex"}}>
-            <div style={{width: "60px", height: "60px", border: "1px solid #000", margin: "5px 5px"}}>5</div>
-            <div style={{width: "60px", height: "60px", border: "1px solid #000", margin: "5px 5px"}}>6</div>
-            <div style={{width: "60px", height: "60px", border: "1px solid #000", margin: "5px 5px"}}>7</div>
-            <div style={{width: "60px", height: "60px", border: "1px solid #000", margin: "5px 5px"}}>8</div>
+            <div style={{width: "60px", height: "60px", border: "1px solid #000", margin: "5px 5px"}}>{props.user.red[0]}</div>
+            <div style={{width: "60px", height: "60px", border: "1px solid #000", margin: "5px 5px"}}>{props.user.red[1]}</div>
+            <div style={{width: "60px", height: "60px", border: "1px solid #000", margin: "5px 5px"}}>{props.user.red[2]}</div>
+            <div style={{width: "60px", height: "60px", border: "1px solid #000", margin: "5px 5px"}}>{props.user.red[3]}</div>
         </div>
     </div>
   )
