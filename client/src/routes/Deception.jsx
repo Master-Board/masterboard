84032f3b6bad5eb2e092042e25a8d30873dcca86
JuHyeongJin <wngudwlswkd@gmail.com>
2022-08-15 15:40:36 +0900

    Add game data, setup game start part

--- client/src/routes/Deception.jsx
+++ client/src/routes/Deception.jsx
@@ -21,6 +21,16 @@ function Deception(props) {
   let userIndex=1;
 
   const [users, setUsers] = useState([{name: "민철", job: null, blue: [12, 42, 53, 13], red: [2, 43, 25, 83]}, {name: "형진", job: null, blue: [54, 62, 11, 40], red: [1, 3, 4, 5]}, {name: "민호", job: null, blue: [6, 7, 8, 9], red: [6, 7, 8, 9]}, {name: "박철", job: null, blue: [10, 11, 12, 13], red: [14, 15, 16, 17]}]);
+  const cause = ['질식', '중상', '과다출혈', '병/질병', '중독', '사고']
+  const place = [['술집', '서점', '식당', '호텔', '병원', '건축부지'], ['별장', '공원', '슈퍼마켓', '학교', '숲', '은행'], ['거실', '침실', '창고', '화장실', '부엌', '발코니']]
+  const hint = [{title: '희생자의 직업', content: ['사장', '전문가', '노동자', '학생', '실직자', '은퇴']}, 
+                {title: '날씨', content: ['화창한', '폭풍우', '건조한', '습한', '추운', '뜨거운']},
+                {title: '인상착의', content: ['흔한', '개성적인', '수상한', '무정한', '무서운', '의심스런']},
+                {title: '시신에 있는 암시', content: ['머리', '가슴', '손', '다리', '시신 토막', '전신']},
+                {title: '하는 중', content: ['유흥', '휴식', '회의', '거래', '방문', '식사']},
+                {title: '시체 상태', content: ['아직 따듯한', '경직된', '부패된', '불구', '멀쩡한', '접질린']},
+                {title: '범죄 일시', content: ['평일', '주말', '봄', '여름', '가을', '겨울']},
+                {title: '범죄 지속 기간', content: ['즉각적', '단시간', '서서히', '장시간', '며칠', '불확실한']}]
   
   useEffect(() => {
     setUser(props.user.id);
@@ -54,7 +64,7 @@ function Deception(props) {
     socket.on("deceptionReady", (data) => {
       if(data) startGame()
     })
-  }, [socket])
+  }, [])
 
   useEffect(() => {
     for(let i = 0; i < users.length; i++){
@@ -63,13 +73,14 @@ function Deception(props) {
 
   }, [users]);
 
+  // 타이머
   useEffect(() => {
     const countdown = setInterval(() => {
       if(parseInt(seconds) > 0) {
         setSeconds(parseInt(seconds) - 1)
       }
       if(parseInt(seconds) === 0) {
-        if(parseInte(minutes) === 0) {
+        if(parseInt(minutes) === 0) {
           clearInterval(countdown)
         }else{
           setMinutes(parseInt(minutes) - 1)
@@ -91,12 +102,18 @@ function Deception(props) {
   };
 
   const Disconnect = () => {
-    socket.disconnect();
+    socket.emit("deceptionLeave", {room})
   }
 
   const startGame = () => {
     console.log("게임시작")
+    setBroadcast("게임이 시작되었습니다.")
     setMyJob(users[userIndex].job);
+
+    //직업 정하기
+    setTimeout(function(){ setBroadcast(`당신은 ${myJob}입니다. 법의학자가 선택을 완료할때까지 기다려주세요.`) }, 1500)
+    
+
     if(myJob == 'god'){ // 법의학자
 
     }else if(myJob == 'murderer'){ // 살인자
@@ -110,6 +127,7 @@ function Deception(props) {
 
   const ready = () => {
     socket.emit("deceptionReady", {player: user, room: room})
+    setBroadcast("준비완료!")
   }
 
   return (
@@ -118,7 +136,7 @@ function Deception(props) {
         <button onClick={()=>setChatting(!chatting)}>채팅</button>
         <span>{broadcast} </span>
         <span>{minutes}:{seconds < 10? `0${seconds}` : seconds}</span>
-        {/* <button onClick={()=>ready}>준비</button> */}
+        <button onClick={()=>ready}>준비</button>
         <button onClick={Disconnect}><Link to="/mainpage">나가기</Link></button>
         <div style={{display: "flex", position: "relative"}}>
           {chatting === true ? 
@@ -145,7 +163,7 @@ function Deception(props) {
             <div className='middle' style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
               {users.length<5? <DeceptionUser user={null}/> : <DeceptionUser user={users[(userIndex+4)%(users.length)]}/>}
               <div style={{width: "800px", height: "250px", margin: "0px 55px", border: "1px solid #111"}}>
-                share
+                
               </div>
               {users.length<6? <DeceptionUser user={null}/> : <DeceptionUser user={users[(userIndex+5)%(users.length)]}/>}
             </div>
