c80f282dd78ab606c6dc9528ce5cd42a6262eaf6
chk223 <104356399+chk223@users.noreply.github.com>
2022-08-26 19:01:35 +0900

    Update socket.js

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -1,23 +1,18 @@
 const Socketio = require('socket.io')
 const moment = require('moment')
-let deception_player = []
-let deception_clue_deck //deception의 단서카드 정보 ex) 사용자의 단서카드 값 = 4라면 clue_deck의 5번째 정보를 참조하면 됨.
-let deception_means_deck //deception 수단카드 정보. 위와 마찬가지.
-let deception_clue // deception에 실제 사용되는 단서카드
-let deception_means // deception에 실제 사용되는 단서카드
-let deception_murderer //살인자 플레이어 닉네임 혹은 번호
-let deception_murder_means // 살인자 수단카드 번호
-let deception_murder_clue //살인자 단서카드 번호
-let deception_info = [{room:0}, {deception_player: []} ,{deception_clue_deck}, {deception_means_deck}, 
-{deception_clue},{deception_means},{deception_murderer}, {deception_murder_means}, {deception_murder_clue}]
-function swap(a,b) {
-    let tmp = a
-    a = b
-    b = tmp
-    return a,b
-}
+var deception_player = []
+var deception_clue_deck //deception의 단서카드 정보 ex) 사용자의 단서카드 값 = 4라면 clue_deck의 5번째 정보를 참조하면 됨.
+var deception_means_deck //deception 수단카드 정보. 위와 마찬가지.
+var deception_clue // deception에 실제 사용되는 단서카드
+var deception_means // deception에 실제 사용되는 단서카드
+var deception_murderer //살인자 플레이어 닉네임 혹은 번호
+var deception_murder_means // 살인자 수단카드 번호
+var deception_murder_clue //살인자 단서카드 번호
+var deception_info = []
+// let deception_info = [{room:0}, {deception_player: []} ,{deception_clue_deck}, {deception_means_deck}, 
+// {deception_clue},{deception_means},{deception_murderer}, {deception_murder_means}, {deception_murder_clue}]
 function deception_init_clueinfo(){//단서카드 정보 return
-    let clue_deck = {}
+    let clue_deck = []
     clue_deck[0] = '에어컨'
     clue_deck[1] = '개미'
     clue_deck[2] = '골동품'
@@ -38,11 +33,92 @@ function deception_init_clueinfo(){//단서카드 정보 return
     clue_deck[17] = '버튼'
     clue_deck[18] = '케이크'
     clue_deck[19] = '달력'
-    clue_deck[20] = '사탕'
+    clue_deck[20] = '선글라스'
+    clue_deck[21] = '우유통'
+    clue_deck[22] = '카세트 테이프'
+    clue_deck[23] = '고양이'
+    clue_deck[24] = '신분증'
+    clue_deck[25] = '분필'
+    clue_deck[26] = '담배'
+    clue_deck[27] = '티슈'
+    clue_deck[28] = '톱밥'
+    clue_deck[29] = '세제'
+    clue_deck[30] = '바퀴벌레'
+    clue_deck[31] = '커피'
+    clue_deck[32] = '동전'
+    clue_deck[33] = '만화책'
+    clue_deck[34] = '컴퓨터'
+    clue_deck[35] = 'USB'
+    clue_deck[36] = '키보드 마우스'
+    clue_deck[37] = '밀서'
+    clue_deck[38] = '마스크'
+    clue_deck[39] = '천'
+    clue_deck[40] = '컵'
+    clue_deck[41] = '커튼'
+    clue_deck[42] = '다이아몬드'
+    clue_deck[43] = '틀니'
+    clue_deck[44] = '다이어리'
+    clue_deck[45] = '주사위'
+    clue_deck[46] = '사전'
+    clue_deck[47] = '비누'
+    clue_deck[48] = '문서'
+    clue_deck[49] = '먼지'
+    clue_deck[50] = '귀걸이'
+    clue_deck[51] = '달걀'
+    clue_deck[52] = '전기회로'
+    clue_deck[53] = '봉투'
+    clue_deck[54] = '시험지'
+    clue_deck[55] = '선풍기'
+    clue_deck[56] = '팩스'
+    clue_deck[57] = '광섬유'
+    clue_deck[58] = '손톱'
+    clue_deck[59] = '손전등'
+    clue_deck[60] = '십자 드라이버'
+    clue_deck[61] = '식재료'
+    clue_deck[62] = '기어'
+    clue_deck[63] = '장갑'
+    clue_deck[64] = '접착제'
+    clue_deck[65] = '낙서'
+    clue_deck[66] = '헤어핀'
+    clue_deck[67] = '옷걸이'
+    clue_deck[68] = '모자'
+    clue_deck[69] = '수갑'
+    clue_deck[70] = '헤드셋'
+    clue_deck[71] = '하이힐'
+    clue_deck[72] = '모래시계'
+    clue_deck[73] = '얼음'
+    clue_deck[74] = '잉크'
+    clue_deck[75] = '곤충'
+    clue_deck[76] = '쥬스'
+    clue_deck[77] = '열쇠'
+    clue_deck[78] = '나뭇잎'
+    clue_deck[79] = '렌즈'
+    clue_deck[80] = '립스틱'
+    clue_deck[81] = '러브레터'
+    clue_deck[82] = '자물쇠'
+    clue_deck[83] = '로또'
+    clue_deck[84] = '잡지'
+    clue_deck[85] = '휴대폰'
+    clue_deck[86] = '거울'
+    clue_deck[87] = '모기'
+    clue_deck[88] = '목걸이'
+    clue_deck[89] = '노트북'
+    clue_deck[90] = '페인트'
+    clue_deck[91] = '팬티'
+    clue_deck[92] = '토끼'
+    clue_deck[93] = '모래'
+    clue_deck[94] = '와인'
+    clue_deck[95] = '해골'
+    clue_deck[96] = '스위치'
+    clue_deck[97] = '문신'
+    clue_deck[98] = '우산'
+    clue_deck[99] = '바이올린'
+    clue_deck[100] = '시계'
+
     return clue_deck
 }
 function deception_init_meansinfo() { //수단카드 정보
-    let means_deck = {}
+    let means_deck = []
     means_deck[0] = '알코올'
     means_deck[1] = '아메바'
     means_deck[2] = '비소(가루약물)'
@@ -64,99 +140,172 @@ function deception_init_meansinfo() { //수단카드 정보
     means_deck[18] = '목발'
     means_deck[19] = '단도'
     means_deck[20] = '더러운 물'
+    means_deck[21] = '절단'
+    means_deck[22] = '드릴'
+    means_deck[23] = '익사'
+    means_deck[24] = '아령'
+    means_deck[25] = '전기자전거'
+    means_deck[26] = '전기 배턴'
+    means_deck[27] = '감전'
+    means_deck[28] = '폭발물'
+    means_deck[29] = '접이식 의자'
+    means_deck[30] = '화약'
+    means_deck[31] = '망치'
+    means_deck[32] = '갈고리'
+    means_deck[33] = '스케이트화'
+    means_deck[34] = '불법 마약'
+    means_deck[35] = '약물 투여'
+    means_deck[36] = '등유'
+    means_deck[37] = '발차기'
+    means_deck[38] = '나이프와 포크'
+    means_deck[39] = '라이터'
+    means_deck[40] = '잠긴 문'
+    means_deck[41] = '마체테'
+    means_deck[42] = '기계'
+    means_deck[43] = '광견'
+    means_deck[44] = '성냥'
+    means_deck[45] = '수은'
+    means_deck[46] = '쇠사슬'
+    means_deck[47] = '과식'
+    means_deck[48] = '포장 테이프'
+    means_deck[49] = '살충제'
+    means_deck[50] = '알약'
+    means_deck[51] = '베개'
+    means_deck[52] = '권총'
+    means_deck[53] = '전염병'
+    means_deck[54] = '비닐봉지'
+    means_deck[55] = '독성 가스'
+    means_deck[56] = '독침'
+    means_deck[57] = '가루 약물'
+    means_deck[58] = '화분'
+    means_deck[59] = '주먹질'
+    means_deck[60] = '밀기'
+    means_deck[61] = '방사능'
+    means_deck[62] = '광선검'
+    means_deck[63] = '밧줄'
+    means_deck[64] = '스카프'
+    means_deck[65] = '가위'
+    means_deck[66] = '조각상'
+    means_deck[67] = '연기'
+    means_deck[68] = '저격'
+    means_deck[69] = '굶주림'
+    means_deck[70] = '쇠파이프'
+    means_deck[71] = '돌멩이'
+    means_deck[72] = '황산'
+    means_deck[73] = '수술'
+    means_deck[74] = '트로피'
+    means_deck[75] = '모종삽'
+    means_deck[76] = '맨손'
+    means_deck[77] = '독사'
+    means_deck[78] = '독전갈'
+    means_deck[79] = '비디오 게임기'
+    means_deck[80] = '바이러스'
+    means_deck[81] = '채찍'
+    means_deck[82] = '낚시줄'
+    means_deck[83] = '과로'
+    means_deck[84] = '스패너'
+    means_deck[85] = '깨물기'
     return means_deck
 }
 function deception_init_card(card_count) { //실제 사용 덱 숫자 기입
-    let deck = new Array(card_count)
+    let deck = []
     for(let i=0;i<card_count;i++) {
         deck[i] = i
     }
     return deck
 }
-function deception_init_game(c,m) { // 처음 게임 시작할 때 덱 구성
-    c = deception_init_clueinfo()
-    m = deception_init_meansinfo()
-    clue = deception_init_card(c.length)
-    clue = deception_shuffle(clue)
-    means = deception_init_card(m.length)
-    means = deception_shuffle(means)
-    return [clue,means]
-}
 function deception_shuffle(deck) {    //실제 사용 덱 숫자 셔플
+    let randnum
     for(let i=0; i<deck.length; i++){
-        let randnum = Math.random()*20
-        deck[i],deck[randnum] = swap(deck[i],deck[randnum])
+        randnum = Math.floor(Math.random()*20)
+        let tmp = deck[i]
+        deck[i] = deck[randnum]
+        deck[randnum] = tmp
     }
     return deck
 }
-function deception_draw_card(deck,card_count) {
-    let card = []
-    if(deck.length <= 0) {//사실 이 경우는 없음. 있다면 오류.
-        console.log('덱이 비어있음.')
+function deception_init_game(deception_clue_deck,deception_means_deck, deception_clue, deception_means) { // 처음 게임 시작할 때 덱 구성
+    deception_clue_deck = deception_init_clueinfo()
+    //console.log('1번: ',deception_clue_deck)
+    deception_means_deck = deception_init_meansinfo()
+    //console.log('2번: ',deception_means_deck)
+    //console.log('clue length:',deception_clue_deck.length)
+    deception_clue = deception_init_card(deception_clue_deck.length)
+    deception_clue = deception_shuffle(deception_clue)
+    //console.log('clue deck: ',deception_clue)
+    deception_means = deception_init_card(deception_means_deck.length)
+    deception_means = deception_shuffle(deception_means)
+    //console.log('means deck: ', deception_means)
+    return [deception_clue_deck,deception_means_deck,deception_clue,deception_means]
+}
+function deception_draw_card(deception_clue_deck,deception_means_deck,deception_clue,deception_means,card_count) {
+    let clue = []
+    let means = []
+    let tmp
+    //console.log('남은 패?',deception_clue)
+    for(let i=0;i<card_count;i++){
+        tmp = deception_clue.pop()
+        clue.push(deception_clue_deck[tmp])
+        tmp = deception_means.pop()
+        means.push(deception_means_deck[tmp])
     }
-    else if(deck.length ==1) {
-        console.log('덱을 모두 소모하였음. 초기화 하겠음.')//사실 이 경우도 없음.
-        card = deck.pop()
-        deck = deception_init_card(deck.length)
-        deck = deception_shuffle(deck)
-        return card, deck
-    }
-    else {//덱에 오류가 없다면 실행
-        for(let i=0;i<card_count;i++){
-            card.push(deck.pop())
-        }
-        return [card,deck]
-    }
+    //console.log('clue: ',clue,' and means: ', means)
+    return [clue, means]    
 }
 function deception_decide_role(personnel,deception_player) {//랜덤으로 역할 정하기
     let randnum
-    let god //법의학자, 1명
-    let murderer // 살인자, 1명
-    let witness //목격자
-    let confederate = [] //공범자
     let player = []
     for(let i=0;i<personnel;i++){
-        player.push(deception_player[i].socketId)//**나중에 닉네임이나 이름 등으로 변경할 것!
+        player[i] = i
     }
-    randnum = Math.random()*personnel
-    god = player[randnum].job
+    randnum = Math.floor(Math.random()*personnel)
+    // console.log('d:', deception_player)
+    // console.log('p:',player)
+    // console.log('god: ',randnum)
+    // console.log('p 1:',player)
+    deception_player[player[randnum]].job = '법의학자'
+    //console.log('이사람이 god.',deception_player[player[randnum]].name)
     player.splice(randnum,1)
-    deception_player[randnum].job = 'god'
     personnel-=1
-    randnum = Math.random()*personnel
-    murderer = player[randnum]
-    deception_player[randnum].job = 'murderer'
+    randnum = Math.floor(Math.random()*personnel)
+    //console.log('murderer: ',randnum)
+    deception_player[player[randnum]].job = '살인자'
+    //console.log('이사람이 murderer.',deception_player[player[randnum]].name)
     player.splice(randnum,1)
+    //console.log('p 2:',player)
     personnel -=1
     if(personnel>3) {//총 인원 6명 이상
-        randnum = Math.random()*personnel
-        witness = player[randnum]
-        deception_player[randnum].job = 'witness'
+        randnum = Math.floor(Math.random()*personnel)
+        //console.log('witness: ',randnum)
+        deception_player[player[randnum]].job = '목격자'
+        //console.log('이사람이 witness.',deception_player[player[randnum]].name)
         player.splice(randnum,1)
+        //console.log('p 3:',player)
         personnel-=1    
         if(personnel>=3) {//위에서 3명을 빼고 남은 인원 3 총 인원 6~7 / 공범 1
-            randnum = Math.random()*personnel
-            confederate.push(player[randnum])
-            deception_player[randnum].job = 'confederate'
+            randnum = Math.floor(Math.random()*personnel)
+            //console.log('confederate: ',randnum)
+            deception_player[player[randnum]].job = '공범자'
+            //console.log('이사람이 confederate.',deception_player[player[randnum]].name)
             player.splice(randnum,1)
-            personnel-=1
+            //console.log('p 4:',player)
         }
         else if(personnel>=5) {//총 인원 8~9 / 공범 2
             for(let i=0;i<2;i++){
-                randnum = Math.random()*personnel
-                confederate.push(player[randnum])
-                deception_player[randnum].job = 'confederate'
+                randnum = Math.floor(Math.random()*personnel)
+                //console.log('confederate: ',randnum)
+                deception_player[player[randnum]].job = '공범자'
                 player.splice(randnum,1)
-                personnel-=1
+                //console.log('p 4:',player)
             }
         }
         else if(personnel==7) {//총 인원 10 / 공범 3
             for(let i=0;i<3;i++){
-                randnum = Math.random()*personnel
-                confederate.push(player[randnum])
-                deception_player[randnum].job = 'confederate'
+                randnum = Math.floor(Math.random()*personnel)
+                //console.log('confederate: ',randnum)
+                deception_player[player[randnum]].job = '공범자'
                 player.splice(randnum,1)
-                personnel-=1
+                //console.log('p 4:',player)
             }
         }
         else {//인원초과.
@@ -213,13 +362,56 @@ module.exports = (server) => {
     
         socket.on('deceptionJoin',(data) => {
             let {room, name} = data
+            //let card_count = 4
             console.log('data: ',data)
             let player_form = {name: name, ready: false, job: 'detective', clue: [], means: []}
             if (data.room != '') {
                 socket.join(room)
 
                 console.log(room+'번 방 join 완료!')
+
                 deception_player.push(player_form)
+
+                //가상으로 인원 설정 test
+
+                // player_form = {name: '형진', ready: false, job: 'detective', clue: [], means: []}
+                // deception_player.push(player_form)
+                // player_form = {name: '창현', ready: false, job: 'detective', clue: [], means: []}
+                // deception_player.push(player_form)
+                // player_form = {name: '철', ready: false, job: 'detective', clue: [], means: []}
+                // deception_player.push(player_form)
+                // player_form = {name: '민호', ready: false, job: 'detective', clue: [], means: []}
+                // deception_player.push(player_form)
+                // player_form = {name: 'JuD', ready: false, job: 'detective', clue: [], means: []}
+                // deception_player.push(player_form)
+                // player_form = {name: 'Looser', ready: false, job: 'detective', clue: [], means: []}
+                // deception_player.push(player_form)
+                //console.log('deception_player: ',deception_player)
+
+                //가상으로 인원 설정 test
+
+                // start의 덱 설정 test
+                // let personnel = deception_player.length
+                // //console.log('personnel: ',personnel)
+                // //초기 덱 설정
+                // let init_deck = deception_init_game(deception_clue_deck,deception_means_deck,deception_clue,deception_means)
+                // //console.log('init_deck: ',init_deck)
+                // deception_clue_deck = init_deck[0]
+                // deception_means_deck = init_deck[1]
+                // deception_clue = init_deck[2]
+                // deception_means = init_deck[3]
+                // //console.log('초기 덱 설정 끝',deception_clue, deception_means)                
+                // //직업정하기
+                // deception_player = deception_decide_role(personnel,deception_player)
+                // //카드뽑기
+                // for(let i=0;i<personnel;i++) {
+                //     let drawn_card = deception_draw_card(deception_clue_deck,deception_means_deck,deception_clue,deception_means,card_count)
+                //     //console.log('drawn_card: ',drawn_card)
+                //     deception_player[i].clue = drawn_card[0]
+                //     deception_player[i].means = drawn_card[1]
+                // }
+
+                // start의 덱 설정 test
 
                 console.log('플레이어: ',deception_player)
 
@@ -280,16 +472,22 @@ module.exports = (server) => {
     
         socket.on('deceptionStart',(data) => {//게임 시작(카드 덱 설정)
             let {room,card_count} = data
+            
             let personnel = deception_player.length
-            //초기 덱 설정
-            [deception_clue_deck, deception_means_deck] = deception_init_game()
-            //직업정하기
-            deception_player = deception_decide_role(personnel,deception_player)
-            //카드뽑기
-            for(let i=0;i<personnel;i++) {
-                [deception_clue, deception_player[i].clue] = deception_draw_card(deception_clue,card_count)
-                [deception_means, deception_player[i].means] = deception_draw_card(deception_means,card_count)
-            }
+                //초기 덱 설정
+                let init_deck = deception_init_game(deception_clue_deck,deception_means_deck,deception_clue,deception_means)
+                deception_clue_deck = init_deck[0]
+                deception_means_deck = init_deck[1]
+                deception_clue = init_deck[2]
+                deception_means = init_deck[3]             
+                //직업정하기
+                deception_player = deception_decide_role(personnel,deception_player)
+                //카드뽑기
+                for(let i=0;i<personnel;i++) {
+                    let drawn_card = deception_draw_card(deception_clue_deck,deception_means_deck,deception_clue,deception_means,card_count)
+                    deception_player[i].clue = drawn_card[0]
+                    deception_player[i].means = drawn_card[1]
+                }
             socket.to(room).emit('deceptionStart', {
                 deception_player
             })            
@@ -320,12 +518,9 @@ module.exports = (server) => {
         })
     
         socket.on('deceptionGuessAnswer',(data) =>{ //최후의 추리
-            let room = data.room
-            let guess_murderer = data.murderer
-            let guess_means = data.means
-            let guess_clue = data.clue
+            const {room, murderer, means, clue} = data
             let anwer
-            if (guess_murderer == deception_murderer && guess_means == deception_murder_means && guess_clue == deception_murder_clue) {
+            if (murderer == deception_murderer && means == deception_murder_means && clue == deception_murder_clue) {
                 anwer = 'right answer'
                 socket.to(room).emit('deceptionGuessAnswer', {anwer})
             }
